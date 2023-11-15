00297e151771e64e227be76bdcc69cb8c9d19c75
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-15 12:04:08 +0100

    Initial commit

---
+++ exinfos.ex
@@ -0,0 +1,9 @@
+[INFOS]
+gaf=0
+title=WD Bureautique Traitement de texte
+image=description.gif
+description=description.rtf
+source=1
+preview=preview.png
+[OBJECT]
+WD Bureautique Traitement de texte.wdp=1
